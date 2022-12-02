3b6cd1926f468effc688bd4282ba0ce8bd394cf9
Michael Smith <mduron432@gmail.com>
2022-12-01 22:40:21 -0600

    testing server data

--- index.js
+++ index.js
@@ -10,9 +10,9 @@
 //         document.getElementById("wrong").style.display = 'block';
 //     }
     
-
+// document.getElementById("login").onclick = function(event){
 fetch('https://shsdata.blob.core.windows.net/moredata/test.txt')
-  .then(response => {
+.then(response => {
     return response.text()
 })
     .then(data => {
@@ -22,7 +22,22 @@ fetch('https://shsdata.blob.core.windows.net/moredata/test.txt')
         console.log(row)
         for(i=0;i<stuffLength;i++){
             if(i % 2 != 0){
-                console.log(stuff[i])
+                username = stuff[i]
+                password = stuff[i+1]
+                console.log(username,password)
             }
-        }
+        } 
     })
+// }
+// function check(username,password){
+    
+    
+//         var username = document.getElementById("username-login").value;
+//         var password = document.getElementById("password-login").value;
+//         if(username == 'mleesers' & password == "88723939Md"){
+//             window.alert("you have been logged in")
+//         }else{
+//             event.preventDefault();
+//             document.getElementById("wrong").style.display = 'block';
+//         }
+// }
