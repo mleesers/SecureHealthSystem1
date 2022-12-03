69db28700acea54e9d1c5155a321deca73608934
Michael Smith <mduron432@gmail.com>
2022-12-03 12:48:02 -0600

    testing server data

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ document.getElementById("login").onclick = function(event){
         if(username == userArr[i] & password == userpassArr[i]){
             valid = true
         }else{
-            console.log(username,password)
+            console.log(userArr[i],userpassArr[i])
             console.log('no')
         }
     }
