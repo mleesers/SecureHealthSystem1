4b28b02b170c558776940e5b30774962856c6043
Michael Smith <mduron432@gmail.com>
2022-12-03 12:06:21 -0600

    testing server data

--- index.js
+++ index.js
@@ -10,34 +10,38 @@
 //         document.getElementById("wrong").style.display = 'block';
 //     }
     
-// document.getElementById("login").onclick = function(event){
+// 
+userArr=[]
+userpassArr=[]
 fetch('https://shsdata.blob.core.windows.net/moredata/users.txt')
 .then(response => {
     return response.text()
 })
     .then(data => {
         stuff = data.split('\n')
-        row = stuff[0]
         stuffLength = stuff.length
-        console.log(row)
         for(i=0;i<stuffLength;i++){
             if(i % 2 == 0){
                 username = stuff[i]
                 password = stuff[i+1]
-                console.log(username,password)
+                userArr.push(username)
+                userpassArr.push(password)
             }
         } 
+        console.log(userArr)
+        console.log(userpassArr)
     })
 // }
-// function check(username,password){
-    
-    
-//         var username = document.getElementById("username-login").value;
-//         var password = document.getElementById("password-login").value;
-//         if(username == 'mleesers' & password == "88723939Md"){
-//             window.alert("you have been logged in")
-//         }else{
-//             event.preventDefault();
-//             document.getElementById("wrong").style.display = 'block';
-//         }
-// }
+// function check(username,password){ 
+document.getElementById("login").onclick = function(event){
+    var username = document.getElementById("username-login").value;
+    var password = document.getElementById("password-login").value;
+    for(i=0;i<userArr.length;i++){
+        if(username == userArr[i] & password == userpassArr[i]){
+            window.alert("you have been logged in")
+        }else{
+            event.preventDefault();
+        }
+    document.getElementById("wrong").style.display = 'block';
+    }
+}
