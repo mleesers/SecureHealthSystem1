b4022deb74e2af8aad89d41da1f9cf524cc2a3d4
Michael Smith <mduron432@gmail.com>
2022-12-04 13:21:02 -0600

    testing server data

--- index.js
+++ index.js
@@ -1,40 +1,27 @@
-// /*
-// document.getElementById("login").onclick = function(event){
-    
-//     var username = document.getElementById("username-login").value;
-//     var password = document.getElementById("password-login").value;
-//     if(username == 'mleesers' & password == "88723939Md"){
-//        window.alert("you have been logged in")
-//     }else{
-//         event.preventDefault();
-//         document.getElementById("wrong").style.display = 'block';
-//     }
-    
-// 
-userArr=[]
-userpassArr=[]
-fetch('https://shsdata.blob.core.windows.net/moredata/users.txt')
-.then(response => {
-    return response.text()
-})
-    .then(data => {
-        stuff = data.split('\n')
-        stuffLength = stuff.length
-        for(i=0;i<stuffLength;i++){
-            if(i % 2 == 0){
-                username = stuff[i]
-                password = stuff[i+1]
-                userArr.push(username)
-                userpassArr.push(password)
-            }
-        } 
-        userArr.pop()
-        userpassArr.pop()
-        console.log(userArr)
-        console.log(userpassArr)
-    })
+userArr=['mleesers','cmolina','ssquarepants']
+userpassArr=['88723939Md','MakeupLover369','KrustyKrab123']
+// fetch('https://shsdata.blob.core.windows.net/moredata/users.txt')
+// .then(response => {
+//     return response.text()
+// })
+//     .then(data => {
+//         stuff = data.split('\n')
+//         stuffLength = stuff.length
+//         for(i=0;i<stuffLength;i++){
+//             if(i % 2 == 0){
+//                 username = stuff[i]
+//                 password = stuff[i+1]
+//                 userArr.push(username)
+//                 userpassArr.push(password)
+//             }
+//         } 
+//         userArr.pop()
+//         userpassArr.pop()
+//         console.log(userArr)
+//         console.log(userpassArr)
+//     })
 // }
-// function check(username,password){ 
+
 document.getElementById("login").onclick = function(event){
     var username = document.getElementById("username-login").value;
     var password = document.getElementById("password-login").value;
@@ -54,6 +41,6 @@ document.getElementById("login").onclick = function(event){
     }else{
         console.log(valid)
         document.getElementById("wrong").style.display = 'block';
-        //event.preventDefault();
+        event.preventDefault();
     }
 }
