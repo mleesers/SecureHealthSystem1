3822ddd2b79bbc37d1545b43df18a22aa7a18197
Michael Smith <mduron432@gmail.com>
2022-12-04 15:23:09 -0600

    testing server data

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-document.getElementById("login").onclick = function(event){
-    userArr=[]
-    userpassArr=[]
-    fetch('https://shsdata.blob.core.windows.net/moredata/users.txt')
-    .then(response => {
-        return response.text()
-    })
-        .then(data => {
-            stuff = data.split('\n')
-            stuffLength = stuff.length
-            for(i=0;i<stuffLength;i++){
-                if(i % 2 == 0){
-                    username = stuff[i]
-                    password = stuff[i+1]
-                    if(typeof username === 'string' & typeof password === 'string'){
-                        username = username.trim();
-                        password = password.trim();
-                        userArr.push(username)
-                        userpassArr.push(password)
+userArr=[]
+userpassArr=[]
+fetch('https://shsdata.blob.core.windows.net/moredata/users.txt')
+.then(response => {
+    return response.text()
+})
+    .then(data => {
+        stuff = data.split('\n')
+        stuffLength = stuff.length
+        for(i=0;i<stuffLength;i++){
+            if(i % 2 == 0){
+                username = stuff[i]
+                password = stuff[i+1]
+                if(typeof username === 'string' & typeof password === 'string'){
+                    username = username.trim();
+                    password = password.trim();
+                    userArr.push(username)
+                    userpassArr.push(password)
+            }
                 }
-                    }
-                    
-            } 
-        })
+                
+        } 
+    })
+
+document.getElementById("login").onclick = function(event){
     var username = document.getElementById("username-login").value;
     var password = document.getElementById("password-login").value;
     valid = false
