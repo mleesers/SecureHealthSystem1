6273e87d28d8bc69eba24cce6984d687db866335
Michael Smith <mduron432@gmail.com>
2022-12-04 16:44:13 -0600

    testing server data

--- payBillResources/payBill.js
+++ payBillResources/payBill.js
@@ -5,17 +5,19 @@ fetch('https://shsdata.blob.core.windows.net/moredata/bankInfo.txt')
 })
 .then(data => {
     stuff = data.split('\n')
-    console.log(stuff)
     stuff[0] = stuff[0].trim()
-    console.log(stuff[0])
     totalBalance = Number(stuff[0])
     bankBalance = Number(stuff[1])
-    arr.push(totalBalance,bankBalance)
     document.getElementById('balance').innerHTML = "$ " + totalBalance
-    
+    doStuff(totalBalance,bankBalance)
 })
+function doStuff(totalBalance,bankBalance){
+    arr.push(totalBalance)
+    arr.push(bankBalance)
+    console.log(arr)
+}
+
+
 totalBalance = arr[0]
 bankBalance = arr[1]
 console.log(totalBalance,bankBalance)
-
-
