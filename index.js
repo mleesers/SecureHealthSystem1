91ee38781d7fd618b475552304262f38d62a073e
Michael Smith <mduron432@gmail.com>
2022-12-04 15:18:59 -0600

    testing server data

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
-userArr=[]
-userpassArr=[]
-fetch('https://shsdata.blob.core.windows.net/moredata/users.txt')
-.then(response => {
-    return response.text()
-})
-    .then(data => {
-        stuff = data.split('\n')
-        stuffLength = stuff.length
-        for(i=0;i<stuffLength;i++){
-            if(i % 2 == 0){
-                username = stuff[i]
-                password = stuff[i+1]
-                if(typeof username === 'string' & typeof password === 'string'){
-                    username = username.trim();
-                    password = password.trim();
-                    userArr.push(username)
-                    userpassArr.push(password)
-            }
-                }
-                
-        } 
-        // userArr.pop()
-        // userpassArr.pop()
-        console.log(userArr)
-        console.log(userpassArr)
-    })
-
 document.getElementById("login").onclick = function(event){
+    userArr=[]
+    userpassArr=[]
+    fetch('https://shsdata.blob.core.windows.net/moredata/users.txt')
+    .then(response => {
+        return response.text()
+    })
+        .then(data => {
+            stuff = data.split('\n')
+            stuffLength = stuff.length
+            for(i=0;i<stuffLength;i++){
+                if(i % 2 == 0){
+                    username = stuff[i]
+                    password = stuff[i+1]
+                    if(typeof username === 'string' & typeof password === 'string'){
+                        username = username.trim();
+                        password = password.trim();
+                        userArr.push(username)
+                        userpassArr.push(password)
+                }
+                    }
+                    
+            } 
+        })
     var username = document.getElementById("username-login").value;
     var password = document.getElementById("password-login").value;
     valid = false
@@ -34,16 +29,12 @@ document.getElementById("login").onclick = function(event){
         if(username == userArr[i] & password == userpassArr[i]){
             valid = true
         }else{
-            console.log(userArr[i],userpassArr[i])
-            console.log(username,password)
             console.log('no')
         }
     }
     if(valid == true){
-        console.log(valid)
         window.alert("you have been logged in")
     }else{
-        console.log(valid)
         document.getElementById("wrong").style.display = 'block';
         event.preventDefault();
     }
