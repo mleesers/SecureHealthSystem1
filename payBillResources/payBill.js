0b44733bf5bd0ad89c440e4b59919dea72ba14cd
Michael Smith <mduron432@gmail.com>
2022-12-04 16:10:28 -0600

    testing server data

--- payBillResources/payBill.js
+++ payBillResources/payBill.js
@@ -14,23 +14,22 @@ fetch('https://shsdata.blob.core.windows.net/moredata/bankInfo.txt')
 
 
 function calculate(totalBalance,bankBalance){
+    document.getElementById('balance').innerHTML = totalBalance
+    document.getElementById("Submit").onclick = function(event){
+
+        Amount = document.getElementById("payamount").value
+    
+        if(Amount <= bankBalance){
+            bankBalance -= Amount
+            totalBalance -= Amount
+            window.alert("Payment of $" + Amount + " was Successfull. Your balance is now $" + totalBalance);
+        }else{
+            window.alert("The bank has declined this payment")
+            event.preventDefault();
+        }
+    
+    };
     console.log(totalBalance,bankBalance)
 }
 
 
-document.getElementById("Submit").onclick = function(event){
-
-    Amount = document.getElementById("payamount").value
-
-    if(Amount <= bankBalance){
-        bankBalance -= Amount
-        totalBalance -= Amount
-        window.alert("Payment of $" + Amount + " was Successfull. Your balance is now $" + totalBalance);
-    }else{
-        window.alert("The bank has declined this payment")
-        event.preventDefault();
-    }
-
-
-    
-};
