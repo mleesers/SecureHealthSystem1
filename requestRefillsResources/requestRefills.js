d26ec1b6ce7fbdc7a1a44cb7bd88ed0a05e4f889
Michael Smith <mduron432@gmail.com>
2022-12-04 17:28:16 -0600

    testing server data

--- requestRefillsResources/requestRefills.js
+++ requestRefillsResources/requestRefills.js
@@ -1,15 +1,27 @@
+medications = []
+descriptions = []
 
-
-
-
-
-
-
-
-
-
-
-
+fetch('https://shsdata.blob.core.windows.net/moredata/medications.txt')
+.then(response => {
+    return response.text()
+})
+.then(data => {
+    stuff = data.split('\n')
+    stuffLength = stuff.length
+    for(i=0;i<stuffLength;i++){
+        if(i % 2 == 0){
+            medication = stuff[i]
+            description = stuff[i+1]
+            if(typeof username === 'string' & typeof password === 'string'){
+                medication = medication.trim();
+                description = description.trim();
+                medications.push(medication)
+                descriptions.push(description)
+        }
+            }
+            
+    } 
+})
 
 
 document.getElementById("submit").onclick = function(){
