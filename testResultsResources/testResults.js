0f1a2f0edaf2a5132d3147a1e33f144a734fca5c
Michael Smith <mduron432@gmail.com>
2022-12-04 19:46:30 -0600

    testing server data

--- testResultsResources/testResults.js
+++ testResultsResources/testResults.js
@@ -1,7 +1,33 @@
 datePage = document.getElementById('section1')
 testResults = document.getElementById('section2')
 
-
+dates = []
+test = []
+var medication
+fetch('https://shsdata.blob.core.windows.net/moredata/dates.txt')
+.then(response => {
+    return response.text()
+})
+.then(data => {
+    stuff = data.split('\n')
+    console.log(stuff)
+    stuffLength = stuff.length
+    // for(i=0;i<stuffLength;i++){
+    //     if(i % 2 == 0){
+    //         medication = stuff[i]
+    //         description = stuff[i+1]
+    //         if(typeof medication === 'string' & typeof description === 'string'){
+    //             medication = medication.trim();
+    //             description = description.trim();
+    //             medications.push(medication)
+    //             descriptions.push(description)
+    //          }
+    //     }   
+    // } 
+    // document.getElementById('name').innerHTML = medications[0]
+    // document.getElementById('name1').innerHTML = medications[1]
+    // document.getElementById('name2').innerHTML = medications[2]
+})
 document.getElementById("date1").onclick = function(){
     datePage.style.display = 'none'
     testResults.style.display = 'block'   
